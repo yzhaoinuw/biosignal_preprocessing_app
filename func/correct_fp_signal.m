09110b524b4c919623811fe692c69d977e2a1424
yzhaoinuw <yzhaoinuw@gmail.com>
2025-05-08 01:48:48 -0400

    Add Option to Invert a Signal

--- func/correct_fp_signal.m
+++ func/correct_fp_signal.m
@@ -1,10 +1,15 @@
-function signal = correct_fp_signal(bioSignal, refSignal, rangeInd, meanFilterOrder)
+function signal = correct_fp_signal(bioSignal, refSignal, opts)
     arguments
         bioSignal (1,:) double
         refSignal (1,:) double
-        rangeInd (1,:) double = (1:length(bioSignal));
-        meanFilterOrder (1,1) double = 1000
+        opts.rangeInd (1,:) double = 1:length(bioSignal)
+        opts.meanFilterOrder (1,1) double = 1000
+        opts.invert (1,1) logical = false
     end
+
+    rangeInd = opts.rangeInd;
+    meanFilterOrder = opts.meanFilterOrder;
+    invert = opts.invert;
 
     MeanFilter = ones(meanFilterOrder,1)/meanFilterOrder;  
     reg = polyfit(refSignal(rangeInd), bioSignal(rangeInd), 1);
@@ -14,6 +19,9 @@ function signal = correct_fp_signal(bioSignal, refSignal, rangeInd, meanFilterOr
     controlFit =  filtfilt(MeanFilter,1,controlFit);
     normDat = (bioSignal - controlFit)./controlFit;
     deltaSignal = normDat * 100;
+    if invert
+        deltaSignal = deltaSignal*(-1);
+    end
     % smoothing traces
     signal = filtfilt(MeanFilter,1,deltaSignal);
 end
