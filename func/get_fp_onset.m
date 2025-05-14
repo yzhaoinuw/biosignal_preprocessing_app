1cc841f9ed46daa2a20bea98833e749576ce6a58
yzhaoinuw <yzhaoinuw@gmail.com>
2025-05-14 14:34:43 -0400

    Make OnsetFP to be at Least 1
    1. Make OnsetFP at least 1 since it may be rounded to 0.
    2. Remove Done processing Viewpoint message if no Viewpoint data.

--- func/get_fp_onset.m
+++ func/get_fp_onset.m
@@ -1,4 +1,5 @@
 function onsetFP = get_fp_onset(TDTData, ttlChannel, fpFrequency)
+    onsetFP = 1;
     if ~strcmp(ttlChannel, 'none')
         ttlFP = TDTData.epocs.(ttlChannel).onset;
         ttlGap = diff(ttlFP) > 6;
@@ -8,7 +9,6 @@ function onsetFP = get_fp_onset(TDTData, ttlChannel, fpFrequency)
             ttlOnset = ttlFP(find(ttlGap==1)+1); % when TTL pulse train is started more than once
         end
         onsetFP = round(ttlOnset(1)*fpFrequency); 
-    else
-        onsetFP = 1;
     end
+    onsetFP = max(onsetFP, 1); % index starts at least 1 
 end
