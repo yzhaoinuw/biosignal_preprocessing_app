acd39012d0fee34fba02ef8728a00697a1c0db4e
yzhaoinuw <yzhaoinuw@gmail.com>
2025-10-13 01:52:05 -0400

    Add Event Label Extraction
    1. Extracts and saves event labels if recorded in an apocs channel.
    2. Add fp frequency when saving mat.

--- func/get_fp_onset.m
+++ func/get_fp_onset.m
@@ -1,14 +1,13 @@
-function onsetFP = get_fp_onset(TDTData, ttlChannel, fpFrequency)
-    onsetFP = 1;
+function ttlOnset = get_fp_onset(TDTData, ttlChannel)
+    ttlOnset = 0;
     if ~strcmp(ttlChannel, 'none')
         ttlFP = TDTData.epocs.(ttlChannel).onset;
         ttlGap = diff(ttlFP) > 6;
         if isempty(find(ttlGap == 1, 1))
             ttlOnset = ttlFP(1);  % when TTL pulse train is only started once
         else 
-            ttlOnset = ttlFP(find(ttlGap==1)+1); % when TTL pulse train is started more than once
+            ttlOnset = ttlFP(find(ttlGap==1, 1)+1); % when TTL pulse train is started more than once
         end
-        onsetFP = round(ttlOnset(1)*fpFrequency); 
+        %onsetFP = round(ttlOnset(1)*fpFrequency); 
     end
-    onsetFP = max(onsetFP, 1); % index starts at least 1 
 end
